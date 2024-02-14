086bde653f3d02416a677b7f7c3e80e2ae7439fa
cod2048 <cod2048@gmail.com>
2024-02-14 23:43:45 +0900

    init: first commit

---
+++ src/main/java/com/hanghae/preorder_project/PreorderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.preorder_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreorderProjectApplication.class, args);
+    }
+
+}
