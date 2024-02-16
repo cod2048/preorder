108c4955966cd56f13051dbbbb00134bd53116a5
cod2048 <cod2048@gmail.com>
2024-02-16 16:43:06 +0900

    feat #1 : 유저 생성 요청 dto 작성

---
+++ module_user/src/main/java/com/hanghae/module_user/user/dto/request/CreateUserRequest.java
@@ -0,0 +1,18 @@
+package com.hanghae.module_user.user.dto.request;
+
+import com.hanghae.module_user.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CreateUserRequest {
+    private String email;
+    private String password;
+    private String name;
+    private User.UserRole userRole;
+}
