0efee2f3345e04aa6fe5197611ced6cc7ed8d423
cod2048 <cod2048@gmail.com>
2024-02-16 16:43:47 +0900

    feat #1 : 유저 엔티티 수정
    - 생성
    - 수정
    - 비밀번호 업데이트
    - 삭제

--- module_user/src/main/java/com/hanghae/module_user/user/entity/User.java
+++ module_user/src/main/java/com/hanghae/module_user/user/entity/User.java
@@ -1,11 +1,16 @@
 package com.hanghae.module_user.user.entity;
 
+import com.hanghae.module_user.user.dto.request.CreateUserRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -29,6 +34,17 @@ public class User {
     @Column(name = "role", nullable = false)
     private UserRole userRole;
 
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
     @Builder
     public User(Long userNum, String email, String password, String name, UserRole userRole) {
         this.userNum = userNum;
@@ -38,9 +54,33 @@ public class User {
         this.userRole = userRole;
     }
 
+    public static User create(CreateUserRequest createUserRequest, String encodedPassword) {
+
+        return User.builder()
+                .email(createUserRequest.getEmail())
+                .password(encodedPassword)
+                .name(createUserRequest.getName())
+                .userRole(createUserRequest.getUserRole())
+                .build();
+    }
+
+    public void update(String name) {
+        this.name = name;
+    }
+
+    public void updatePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
     public enum UserRole {
         SELLER,
         BUYER
     }
 
 }
+
+
