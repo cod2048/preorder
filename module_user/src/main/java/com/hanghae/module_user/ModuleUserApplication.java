c5d7fbf318e36321dad6749ed46dfc68e1f3a927
cod2048 <cod2048@gmail.com>
2024-02-16 15:50:43 +0900

    feat #1 : jpa, feign 어노테이션 추가

--- module_user/src/main/java/com/hanghae/module_user/ModuleUserApplication.java
+++ module_user/src/main/java/com/hanghae/module_user/ModuleUserApplication.java
@@ -2,8 +2,12 @@ package com.hanghae.module_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class ModuleUserApplication {
 
     public static void main(String[] args) {
