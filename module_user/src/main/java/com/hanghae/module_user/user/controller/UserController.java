ec5bd2e1f97ca677babc9d026c6b63d0f3a87689
cod2048 <cod2048@gmail.com>
2024-02-16 19:34:17 +0900

    feat #1 : 회원가입 기능 구현

---
+++ module_user/src/main/java/com/hanghae/module_user/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.hanghae.module_user.user.controller;
+
+import com.hanghae.module_user.user.dto.request.CreateUserRequest;
+import com.hanghae.module_user.user.entity.User;
+import com.hanghae.module_user.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signUp(@RequestBody CreateUserRequest createUserRequest) {
+        return ResponseEntity.ok(userService.create(createUserRequest));
+    }
+
+}
