bbb23da4e61dd3c8a8a8b0418f98e180d9dd29f0
cod2048 <cod2048@gmail.com>
2024-02-19 03:53:02 +0900

    init #4 : 상품 모듈 생성

---
+++ module_item/src/main/java/com/hanghae/module_item/ModuleItemApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.module_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleItemApplication.class, args);
+    }
+
+}
---
+++ module_item/src/test/java/com/hanghae/module_item/ModuleItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.module_item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
