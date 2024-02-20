d64eeb84393da7df8d21817e00f34aefed63a971
cod2048 <cod2048@gmail.com>
2024-02-20 20:34:28 +0900

    init #6 : 프로젝트 초기설정

---
+++ modlue_payment/src/main/java/com/hanghae/modlue_payment/ModluePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.modlue_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModluePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModluePaymentApplication.class, args);
+	}
+
+}
---
+++ modlue_payment/src/test/java/com/hanghae/modlue_payment/ModluePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.modlue_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModluePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
