a0dc457e896086da41014d200227d56de6bd44aa
cod2048 <cod2048@gmail.com>
2024-02-21 02:00:15 +0900

    feat #6 : 주문 정보 생성 기능 구현

---
+++ modlue_payment/src/main/java/com/hanghae/modlue_payment/payment/dto/request/CreatePaymentRequest.java
@@ -0,0 +1,17 @@
+package com.hanghae.modlue_payment.payment.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreatePaymentRequest {
+    private Long orderNum;
+    private Long buyerNum;
+    private Long quantity;
+    private Long price;
+}
