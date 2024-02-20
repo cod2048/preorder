9122a9f2b7e00d1c1fc438c72d07da7ea6574b97
cod2048 <cod2048@gmail.com>
2024-02-20 17:28:40 +0900

    feat #5 : 주문 성공 시 재고 감소

---
+++ module_item/src/main/java/com/hanghae/module_item/item/controller/InternalItemController.java
@@ -0,0 +1,32 @@
+package com.hanghae.module_item.item.controller;
+
+import com.hanghae.module_item.item.dto.request.ReduceStockRequest;
+import com.hanghae.module_item.item.dto.response.StockResponse;
+import com.hanghae.module_item.item.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/internal/items")
+public class InternalItemController {
+    private final ItemService itemService;
+
+
+    public InternalItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/{itemNum}")
+    public ResponseEntity<StockResponse> checkItemStocks(@PathVariable("itemNum") Long itemNum) {
+        Long stocks = itemService.getItemStocks(itemNum);
+        StockResponse stockResponse = new StockResponse(itemNum, stocks);
+
+        return ResponseEntity.ok(stockResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<StockResponse> reduceItemStocks(@RequestBody ReduceStockRequest reduceStockRequest) {
+        StockResponse stockResponse = itemService.reduceItemStocks(reduceStockRequest);
+        return ResponseEntity.ok(stockResponse);
+    }
+}
