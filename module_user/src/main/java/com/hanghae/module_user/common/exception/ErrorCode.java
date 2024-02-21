ebccf8fbb006667dbe0702aede5223b33f37252c
cod2048 <cod2048@gmail.com>
2024-02-22 02:12:38 +0900

    feat #15 : 유저모듈 에러파일 작성

---
+++ module_user/src/main/java/com/hanghae/module_user/common/exception/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.hanghae.module_user.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    // 4xx
+    NOT_FOUND(HttpStatus.BAD_REQUEST, "요청사항을 찾지 못했습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."),
+
+    // Email
+    ALREADY_EXISTS_EMAIL(HttpStatus.BAD_REQUEST,"이미 가입된 이메일입니다."),
+    UNABLE_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패했습니다."),
+
+    // 5xx
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 있습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
