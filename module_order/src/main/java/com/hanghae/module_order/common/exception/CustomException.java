3d0ae371bfb0ddaf30945f5611bb978628f59501
cod2048 <cod2048@gmail.com>
2024-02-22 02:52:54 +0900

    feat #15 : 주문 모듈 커스텀 예외 파일 작성

---
+++ module_order/src/main/java/com/hanghae/module_order/common/exception/CustomException.java
@@ -0,0 +1,17 @@
+package com.hanghae.module_order.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+    private final String message;
+    private final HttpStatus Status;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.Status = errorCode.getStatus();
+    }
+}
---
+++ module_order/src/main/java/com/hanghae/module_order/common/exception/ErrorCode.java
@@ -0,0 +1,32 @@
+package com.hanghae.module_order.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    //4xx
+    NOT_FOUND(HttpStatus.BAD_REQUEST, "요청사항을 찾지 못했습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    NOT_AVAILABLE_TIME(HttpStatus.BAD_REQUEST, "구매 가능 시간이 아닙니다"),
+
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문이 존재하지 않습니다"),
+
+    // 5xx
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 있습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
