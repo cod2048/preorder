261ce94fc80bf398aca941d6250a2839124866a2
cod2048 <cod2048@gmail.com>
2024-02-22 02:22:15 +0900

    feat #15 : 회원가입 시 필수항목 에러코드 추가

--- module_user/src/main/java/com/hanghae/module_user/common/exception/ErrorCode.java
+++ module_user/src/main/java/com/hanghae/module_user/common/exception/ErrorCode.java
@@ -9,6 +9,10 @@ public enum ErrorCode {
 
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."),
+    EMAIL_REQUIRED(HttpStatus.BAD_REQUEST,"이메일은 필수입니다."),
+    NAME_REQUIRED(HttpStatus.BAD_REQUEST,"이름은 필수입니다."),
+    PASSWORD_REQUIRED(HttpStatus.BAD_REQUEST,"비밀번호는 필수입니다."),
+    USER_ROLE_REQUIRED(HttpStatus.BAD_REQUEST,"판매자/구매자 구분은 필수입니다."),
 
     // Email
     ALREADY_EXISTS_EMAIL(HttpStatus.BAD_REQUEST,"이미 가입된 이메일입니다."),
